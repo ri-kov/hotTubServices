ec53d6ae292466b700c1ec95eb2793745dfcf123
ri-kov <rkovalenko1709@gmail.com>
2026-08-08 13:23:04 -0500

    js

--- scripts/script.js
+++ scripts/script.js
@@ -35,3 +35,35 @@ const calendar = document.getElementById("calendar");
 dateButton.addEventListener("click", function () {
     calendar.classList.toggle("open");
 });
+
+const calendarDays = document.getElementById("calendarDays");
+const monthYear = document.getElementById("monthYear");
+
+let currentDate = new Date();
+
+function renderCalendar() {
+    calendarDays.innerHTML = "";
+    const year = currentDate.getFullYear();
+    const month = currentDate.getMonth();
+
+    const firstDayOfMonth = new Date(year, month, 1);
+    const lastDayOfMonth = new Date(year, dateButton, 0);
+
+    const firstWeekday = firstDayOfMonth.getDay();
+    const numberOfDays = lastDayOfMonth.getDate();
+
+    monthYear.textContent = currentDate.toLocaleDateString("en-US", {
+        month: "long",
+        year: "numeric"
+    });
+
+    for (let i = 0; i < numberOfDays; day++) {
+        constdayButton = document.createElement("button");
+
+        dayButton.type = "button";
+        dayButton.textContent = day;
+        dayButton.classList.appendChild(dayButton);
+    }
+}
+
+renderCalendar();
